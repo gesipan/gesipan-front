08981d89381135dd9e9fb12f0e3c9e26756e4ab1
a1p4ca <dreamaker7770@gmail.com>
2018-05-23 21:42:18 +0900

    Welcome

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import 'antd/dist/antd.css'
+import { Button } from 'antd'
+import * as React from 'react'
+
+class App extends React.Component {
+  public render () {
+    return (
+      <div>
+        <Button type='ghost'>Button</Button>
+      </div>
+    )
+  }
+}
+
+export default App
